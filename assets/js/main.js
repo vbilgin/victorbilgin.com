d2cb097aee4d4fb923dc2ca6c1b447875ecc2cb2
Victor Bilgin <hello@victorbilgin.com>
2022-08-16 19:35:06 -0700

    Removed dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,29 +12,3 @@ const changeNav = () => {
 };
 
 window.addEventListener("scroll", changeNav);
-
-// Dark mode
-let darkModeEnabled = localStorage.getItem("isDarkModeEnabled");
-const colorToggleBtn = document.querySelector("#color-toggle");
-
-const toggleColorMode = color => {
-  if (color === "light") {
-    document.body.classList.remove("dark-mode");
-    localStorage.setItem("isDarkModeEnabled", false);
-    colorToggleBtn.src = "/assets/img/icons/sun.svg";
-    colorToggleBtn.alt = "A sun icon";
-  } else if (color === "dark") {
-    document.body.classList.add("dark-mode");
-    localStorage.setItem("isDarkModeEnabled", true);
-    colorToggleBtn.src = "/assets/img/icons/moon.svg";
-    colorToggleBtn.alt = "A moon icon";
-  }
-};
-
-if (darkModeEnabled === "true") toggleColorMode("dark");
-
-colorToggleBtn.addEventListener("click", () => {
-  darkModeEnabled = localStorage.getItem("isDarkModeEnabled");
-  if (darkModeEnabled === "false") toggleColorMode("dark");
-  else toggleColorMode("light");
-});
